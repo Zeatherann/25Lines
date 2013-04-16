e3776372d364a1e99ba51986499161bb0006176a
Michael Jerrick <chaoszinc@mindspring.com>
2013-04-16 03:51:28 -0600

    Initial Commit!

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+// Need main.hpp
+// Pregenerated 4 lines (3 if the #include isn't counted)
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
