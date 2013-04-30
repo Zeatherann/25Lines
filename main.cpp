bd8f299265b8f028fa37fe52f012af2b73348717
Tyler Church <tylertrain@yahoo.com>
2013-04-29 21:06:47 -0700

    The server is listening.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,69 @@
+#include <SFML/System.hpp>
+#include <SFML/Network.hpp>
 #include <iostream>
-// Need main.hpp
-// Pregenerated 4 lines (3 if the #include isn't counted)
+#include <vector>
+#include <functional>
+#include <thread>
 
 using namespace std;
 
+const int PORT = 4567;
+
+enum HOST_OR_JOIN { HOST, JOIN };
+enum EXIT_CODES { UNABLE_TO_LISTEN=1 };
+
+HOST_OR_JOIN HostOrJoin() {
+    cout << "(h)ost or (j)oin: ";
+    char option;
+    cin >> option;
+    if (option == 'h') {
+        return HOST;
+    } else if (option == 'j') {
+        return JOIN;
+    } else {
+        cout << "Invalid input.\n";
+        return HostOrJoin();
+    }
+}
+
+class ClientConnection {
+public:
+    sf::SocketTCP Connection;
+
+    ClientConnection(sf::SocketTCP Connection) : Connection(Connection) {}
+
+    void Run() {
+        cout << "Running a ClientConnection.\n";
+        while (true) {
+        }
+    }
+};
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    HOST_OR_JOIN choice = HostOrJoin();
+    if (choice == HOST) {
+        vector<ClientConnection*> clients;
+
+        sf::SocketTCP listener;
+        if (!listener.Listen(PORT)) {
+            cout << "Error: Unable to listen on port " << PORT << ".\n";
+            exit(UNABLE_TO_LISTEN);
+        }
+
+        while (true) {
+            sf::IPAddress ClientAddress;
+            sf::SocketTCP Socket;
+            if (listener.Accept(Socket, &ClientAddress) != sf::Socket::Done) {
+                cout << "Error: Something went wrong while trying to accept a client connection!\n";
+            }
+            ClientConnection* client = new ClientConnection(Socket);
+            std::function<void()> run = std::bind(&ClientConnection::Run, client);
+            clients.push_back(client);
+            std::thread thread(run);
+        }
+    } else if (choice == JOIN) {
+        cout << "Joining! (Please implement.)\n";
+    }
     return 0;
 }
